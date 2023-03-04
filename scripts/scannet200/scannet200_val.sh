6417a539e0cda6dc9d99bee91becae541523c757
MohamedElAmineBoudjoghra <mohamed.boudjoghra@mbzuai.ac.ae>
2023-03-04 12:42:48 +0400

    EBUI_CC

--- scripts/scannet200/scannet200_val.sh
+++ scripts/scannet200/scannet200_val.sh
@@ -7,7 +7,7 @@ CURR_QUERY=150
 
 # # TRAIN
 python main_instance_segmentation.py \
-general.experiment_name="Oracle" \
+general.experiment_name="EBUI_CC" \
 general.project_name="open_world_instance_segmentation" \
 general.train_mode=true \
 general.checkpoint=null \
