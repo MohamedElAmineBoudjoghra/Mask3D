fe3a655c0a9b625af3396d7ee7e80680d5755130
MohamedElAmineBoudjoghra <mohamed.boudjoghra@mbzuai.ac.ae>
2023-03-11 21:02:48 +0400

    Mask3D_new_order

--- scripts/scannet200/scannet200_val.sh
+++ scripts/scannet200/scannet200_val.sh
@@ -20,7 +20,7 @@ general.clustering_update_mu_iter=20 \
 general.clustering_momentum=0.7 \
 general.c_loss=4 \
 general.store_size=500 \
-general.OW_task == "all_labels"
+general.OW_task="all_labels"
 # # data.num_labels=200 \
 # # general.eval_on_segments=true \
 # # general.train_on_segments=true
